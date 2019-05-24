43081a68ef5eed476a6049d14aa5ec0604f1c7c3
Igor Anokhin <igor.anohin@emlid.com>
2019-05-25 00:04:19 +0300

    what am I doing?

--- app/src/main/java/anokhin/underground/mp3player/MainActivity.kt
+++ app/src/main/java/anokhin/underground/mp3player/MainActivity.kt
@@ -1,22 +1,36 @@
 package anokhin.underground.mp3player
 
 import android.Manifest
+import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0
+    val CUSTOM_DIALOG_ID = 0
+    lateinit var root: File
+    lateinit var curFolder: File
+    lateinit var backToParent: Button
+    lateinit var dialogFilesList: ListView
+    lateinit var currentFolderText: TextView
+    val fileList = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        root = File(Environment.getExternalStorageDirectory().absolutePath)
+        curFolder = root
+
         setContentView(R.layout.activity_main)
         search_button?.setOnClickListener{
 
@@ -63,6 +77,74 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun showFolders() {
+        showDialog(CUSTOM_DIALOG_ID)
+    }
 
+    override fun onCreateDialog(id: Int): Dialog? {
+        var dialog: Dialog? = null
+
+        when (id) {
+            CUSTOM_DIALOG_ID -> {
+                dialog = Dialog(this)
+                dialog.setContentView(R.layout.dialog)
+                dialog.setTitle("File Explorer")
+                dialog.setCancelable(true)
+                dialog.setCanceledOnTouchOutside(true)
+
+                backToParent = dialog.findViewById(R.id.back_to_parent)
+                dialogFilesList = dialog.findViewById(R.id.dialog_files_list)
+                currentFolderText = dialog.findViewById(R.id.current_folder)
+
+                backToParent.setOnClickListener {
+                    listDir(curFolder.parentFile)
+                }
+                dialogFilesList.setOnItemClickListener { parent, view, position, id ->
+                    val selected = File(fileList[position])
+                    if (selected.isDirectory) {
+                        listDir(selected)
+                    } else {
+                        val toast = Toast.makeText(applicationContext, "Only folders may be picked", Toast.LENGTH_LONG)
+                        toast.show()
+                    }
+                }
+
+
+
+            }
+            else -> {
+
+            }
+        }
+
+        return dialog
+    }
+
+    override fun onPrepareDialog(id: Int, dialog: Dialog?) {
+        super.onPrepareDialog(id, dialog)
+        when(id) {
+            CUSTOM_DIALOG_ID -> {
+                listDir(curFolder)
+            }
+            else -> {
+                // nope
+            }
+        }
+    }
+
+    private fun listDir(file: File) {
+        backToParent.isEnabled = !file.equals(root)
+        curFolder = file
+        currentFolderText.text = curFolder.path
+
+        val files = file.listFiles()
+        fileList.clear()
+
+        for (f in files) {
+            fileList.add(f.path)
+        }
+
+        val directoryListAdapter = ArrayAdapter<String>(this,
+            R.layout.simple_list_item_1, fileList)
+        dialogFilesList.adapter = directoryListAdapter
     }
 }
