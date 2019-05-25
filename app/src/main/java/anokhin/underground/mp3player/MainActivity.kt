9dc18ad1e02c7d0197e3af8a8a5d94393d9fb048
Igor Anokhin <igor.anohin@emlid.com>
2019-05-25 15:12:54 +0300

    add aaal changes

--- app/src/main/java/anokhin/underground/mp3player/MainActivity.kt
+++ app/src/main/java/anokhin/underground/mp3player/MainActivity.kt
@@ -2,13 +2,22 @@ package anokhin.underground.mp3player
 
 import android.Manifest
 import android.app.Dialog
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.os.IBinder
+import android.os.RemoteException
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.util.Log
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,9 +32,26 @@ class MainActivity : AppCompatActivity() {
     lateinit var curFolder: File
     lateinit var backToParent: Button
     lateinit var pickFolder: Button
+
+    lateinit var startStopButton: ImageView
+    lateinit var skipTrackButton: ImageView
+    lateinit var singerPhoto: ImageView
+    lateinit var trackName: TextView
+    lateinit var trackTime: TextView
+
     lateinit var dialogFilesList: ListView
     lateinit var currentFolderText: TextView
+
+    private var playerServiceBinder: PlayerService.PlayerServiceBinder? = null
+    private var mediaController: MediaControllerCompat? = null
+    private var callback: MediaControllerCompat.Callback? = null
+    lateinit var serviceConnection: ServiceConnection
+
     val fileList = arrayListOf<String>()
+
+    private fun hasPermission(perm: String): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, perm)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +59,20 @@ class MainActivity : AppCompatActivity() {
         curFolder = root
 
         setContentView(R.layout.activity_main)
+        trackName = findViewById(R.id.track_name)
+        trackTime = findViewById(R.id.song_time)
+        skipTrackButton = findViewById(R.id.skip_track)
+        skipTrackButton.setOnClickListener {
+            if (mediaController != null)
+                mediaController!!.transportControls.skipToNext()
+        }
+        startStopButton = findViewById(R.id.start_stop_button)
+        startStopButton.setOnClickListener {
+            if (mediaController != null)
+                mediaController!!.transportControls.pause()
+        }
+
+
         search_button?.setOnClickListener{
 
             Log.i("Own", "Here")
@@ -53,6 +93,84 @@ class MainActivity : AppCompatActivity() {
                 showFolders()
             }
         }
+
+        callback = object : MediaControllerCompat.Callback() {
+            override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+                if (state == null)
+                    return
+                val playing = state.state == PlaybackStateCompat.STATE_PLAYING
+                skipTrackButton.isClickable = playing
+                startStopButton.isClickable = playing || state.state == PlaybackStateCompat.STATE_PAUSED
+//                if (state.state == PlaybackStateCompat.STATE_PLAYING)
+//                    startStopButton.setImageResource(R.drawable.)
+//                playButton.isEnabled = !playing
+//                pauseButton.isEnabled = playing
+//                stopButton.isEnabled = playing
+            }
+
+            override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+                super.onMetadataChanged(metadata)
+                trackName.text = metadata?.getString(MediaMetadataCompat.METADATA_KEY_TITLE)
+                metadata?.getBitmap(MediaMetadataCompat.METADATA_KEY_ART)
+                metadata?.getString(MediaMetadataCompat.METADATA_KEY_TITLE)
+                metadata?.getString(MediaMetadataCompat.METADATA_KEY_ALBUM)
+                metadata?.getString(MediaMetadataCompat.METADATA_KEY_ARTIST)
+                metadata?.getLong(MediaMetadataCompat.METADATA_KEY_DURATION)
+            }
+        }
+
+        serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName, service: IBinder) {
+                playerServiceBinder = service as PlayerService.PlayerServiceBinder
+                try {
+                    mediaController = MediaControllerCompat(this@MainActivity, playerServiceBinder!!.mediaSessionToken)
+                    mediaController!!.registerCallback(callback!!)
+                    callback!!.onPlaybackStateChanged(mediaController!!.playbackState)
+                } catch (e: RemoteException) {
+                    mediaController = null
+                }
+
+            }
+
+            override fun onServiceDisconnected(name: ComponentName) {
+                playerServiceBinder = null
+                if (mediaController != null) {
+                    mediaController!!.unregisterCallback(callback!!)
+                    mediaController = null
+                }
+            }
+        }
+
+        bindService(Intent(this, PlayerService::class.java), serviceConnection, Context.BIND_AUTO_CREATE)
+
+//        playButton.setOnClickListener {
+//            if (mediaController != null)
+//                if (hasPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//                    playerServiceBinder!!.setMusicRepositoryFolder(Environment.getExternalStorageDirectory().absolutePath + "/Slack")
+//                    Log.i("AmyAPP", "Add new Folder")
+//                }
+//            mediaController!!.transportControls.play()
+//        }
+//
+//        pauseButton.setOnClickListener {
+//            if (mediaController != null)
+//        }
+//
+//        stopButton.setOnClickListener {
+//            if (mediaController != null)
+//                mediaController!!.transportControls.stop()
+//        }
+//
+//        skipToNextButton.setOnClickListener {
+//            if (mediaController != null)
+//                mediaController!!.transportControls.skipToNext()
+//        }
+//
+//        skipToPreviousButton.setOnClickListener {
+//            if (mediaController != null)
+//                mediaController!!.transportControls.skipToPrevious()
+//        }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -99,6 +217,22 @@ class MainActivity : AppCompatActivity() {
 
                 backToParent.setOnClickListener {
                     listDir(curFolder.parentFile)
+                }
+
+                pickFolder.setOnClickListener {
+                    dialog.dismiss()
+                    if (mediaController != null)
+                        if (hasPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            Log.i("AmyAPP", "UNIQ MES. Path: " + curFolder.absolutePath)
+                            try {
+
+                                playerServiceBinder!!.setMusicRepositoryFolder(curFolder.absolutePath)
+                            } catch (err: IllegalArgumentException) {
+                                Log.i("AmyAPP", "Catched")
+                            }
+                            Log.i("AmyAPP", "Add new Folder")
+                        }
+                    mediaController!!.transportControls.play()
                 }
 
                 dialogFilesList.setOnItemClickListener { parent, view, position, id ->
@@ -150,5 +284,15 @@ class MainActivity : AppCompatActivity() {
         val directoryListAdapter = ArrayAdapter<String>(this,
             R.layout.simple_list_item_1, fileList)
         dialogFilesList.adapter = directoryListAdapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        playerServiceBinder = null
+        if (mediaController != null) {
+            mediaController!!.unregisterCallback(callback!!)
+            mediaController = null
+        }
+        unbindService(serviceConnection)
     }
 }
---
+++ app/src/main/java/anokhin/underground/mp3player/MusicRepository.kt
@@ -0,0 +1,82 @@
+package anokhin.underground.mp3player
+
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+import android.util.Log
+
+import java.io.File
+import java.util.ArrayList
+
+class MusicRepository {
+    private val data = ArrayList<Track>()
+    var maxIndex = data.size - 1
+    private var currentItemIndex = 0
+
+    val next: Track?
+        get() {
+            if (currentItemIndex == maxIndex)
+                currentItemIndex = 0
+            else
+                currentItemIndex++
+            return current
+        }
+
+    val previous: Track?
+        get() {
+            if (currentItemIndex == 0)
+                currentItemIndex = maxIndex
+            else
+                currentItemIndex--
+            return current
+        }
+
+    val current: Track?
+        get() = if (maxIndex == -1) null else data[currentItemIndex]
+
+    fun setMusicFolder(musicFolderPath: String) {
+        Log.i("AmyAPP", "foler path $musicFolderPath")
+        val musicFolder = File(musicFolderPath)
+        if (musicFolder.isDirectory)
+            Log.i("AmyAPP", "FOLDER")
+        data.clear()
+        for (file in musicFolder.listFiles()) {
+            if (file.name.endsWith(".mp3"))
+                try {
+                    data.add(Track(file.absolutePath))
+                    Log.i("AmyAPP", "file was created")
+                } catch (err: IllegalArgumentException) {
+                }
+
+        }
+        maxIndex = data.size - 1
+        currentItemIndex = 0
+    }
+
+    class Track {
+
+        var title: String? = null
+            private set
+        var album: String? = null
+            private set
+        var artist: String? = null
+            private set
+        var bitmapResId: Int = 0
+            private set
+        var uri: Uri? = null
+            private set
+        var duration: Long = 0
+            private set // in ms
+
+        constructor(trackPath: String) {
+            Log.i("AmyAPP", "my App reseivec $trackPath")
+            val mmr = MediaMetadataRetriever()
+            mmr.setDataSource(trackPath)
+            this.album = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM)
+            this.title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
+            this.artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+            this.bitmapResId = R.drawable.image396168
+            this.uri = Uri.parse(trackPath)
+            this.duration = java.lang.Long.parseLong(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION))
+        }
+    }
+}
